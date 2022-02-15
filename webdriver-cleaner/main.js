ebd162be4df11ebcb4550955429b5610ecf1920b
✨小透明・宸✨ <1272742391@qq.com>
2022-02-15 23:02:23 +0800

    更新屏蔽扩展

--- webdriver-cleaner/main.js
+++ webdriver-cleaner/main.js
@@ -1,4 +1,1 @@
-Reflect.defineProperty(window.navigator.wrappedJSObject, 'webdriver', {
-    get: exportFunction(() => undefined, window.wrappedJSObject),
-});
-console.log('navigator.webdriver cleaned:', window.navigator.wrappedJSObject.webdriver);
+delete window.navigator.wrappedJSObject.__proto__.webdriver;
